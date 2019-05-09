963356c0fd16189e5f617e0253e4e95b1231d287
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-05-08 21:29:58 -0400

    initial commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Michael Stoffer on 5/7/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
